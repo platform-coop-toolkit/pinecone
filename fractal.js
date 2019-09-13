788cb38d9fa1c225ece6c95a04d796d2cf3008a8
Ned Zimmerman <ned@bight.ca>
2019-09-13 10:47:07 -0600

    Configure Fractal, add Laravel Mix

---
+++ fractal.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/*
+* Require the path module
+*/
+const path = require('path');
+
+/*
+ * Require the Fractal module
+ */
+const fractal = module.exports = require('@frctl/fractal').create();
+
+/*
+ * Give your project a title.
+ */
+fractal.set('project.title', 'Pinecone');
+fractal.set('project.version', 'v1.0.0-dev');
+fractal.set('project.author', 'OCAD University');
+
+/*
+ * Tell Fractal where to look for components.
+ */
+fractal.components.set('path', path.join(__dirname, 'src/components'));
+
+/*
+ * Tell Fractal where to look for documentation pages.
+ */
+fractal.docs.set('path', path.join(__dirname, 'src/docs'));
+
+/*
+ * Tell the Fractal web preview plugin where to look for static assets.
+ */
+fractal.web.set('static.path', path.join(__dirname, 'public'));
+
+/*
+ * Tell the Fractal where static HTML exports should be generated.
+ */
+fractal.web.set('builder.dest', __dirname + '/build');
--- src/docs/01-index.md
+++ src/docs/01-index.md
@@ -1,3 +1,5 @@
-# Pinecone
+---
+title: Pinecone
+---
 
 Pinecone is a pattern library for the Platform Cooperativism Consortium and the Platform Co&#8209;op Resource Library.
