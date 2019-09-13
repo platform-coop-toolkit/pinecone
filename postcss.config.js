216cd4d3ad281bff36815d13591103d7df986c4e
Ned Zimmerman <ned@bight.ca>
2019-09-13 12:10:59 -0600

    Add PostCSS and related configuration

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	plugins: {
+		'postcss-import': {},
+		'postcss-partial-import': {},
+		'postcss-custom-media': {},
+		'postcss-custom-properties': {
+			importFrom: './src/assets/styles/common/_properties.css',
+			preserve: false,
+		},
+		'postcss-calc': {},
+		autoprefixer: true
+	}
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
-const mix = require('laravel-mix');
+const mix = require( 'laravel-mix' );
+require( 'laravel-mix-postcss-config' );
 
-mix.setPublicPath('public')
-   .postCss('src/assets/styles/pinecone.css', 'public/styles/pinecone.css')
-   .js('src/assets/scripts/pinecone.js', 'public/scripts/pinecone.js');
+mix.setPublicPath( 'public' )
+	.postCss( 'src/assets/styles/pinecone.css', 'public/styles/pinecone.css' )
+	.postCssConfig()
+	.js( 'src/assets/scripts/pinecone.js', 'public/scripts/pinecone.js' );
