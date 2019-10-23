c76e137cc22be2995f85c959726c50faf6a4f886
Ned Zimmerman <ned@bight.ca>
2019-10-23 10:31:34 -0400

    Add headings

---
+++ src/components/headings/headings.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	title: 'Headings',
+	status: 'wip',
+	context: {
+		h1: 'Platform Cooperativism',
+		h2: 'How platform co-ops benefit you',
+		h3: 'What is a platform co-op?',
+		h4: 'A tangible alternative'
+	}
+};
