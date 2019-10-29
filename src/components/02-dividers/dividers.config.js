d4550e98ff5b705d99c15ce6cddbb06eb060bbb4
Ned Zimmerman <ned@bight.ca>
2019-10-29 09:51:53 -0600

    Add dividers (fix #27)

---
+++ src/components/02-dividers/dividers.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+	title: 'Dividers',
+	status: 'wip',
+	context: {
+		dividersTitle: 'Dividers',
+		dividers: [
+			{
+				name: 'Default',
+				class: ''
+			},
+			{
+				name: 'Grey 400 Hairline',
+				class: 'is-style-grey-400-hairline'
+			},
+			{
+				name: 'Grey 400 Thick',
+				class: 'is-style-grey-400-thick'
+			},
+			{
+				name: 'Blue 500 Hairline',
+				class: 'is-style-blue-500-hairline'
+			},
+			{
+				name: 'Blue 500 Medium',
+				class: 'is-style-blue-500-medium'
+			},
+			{
+				name: 'Blue 600 Hairline',
+				class: 'is-style-blue-600-hairline'
+			},
+			{
+				name: 'Dark Mint 500 Hairline',
+				class: 'is-style-dark-mint-500-hairline'
+			}
+		]
+	}
+};
