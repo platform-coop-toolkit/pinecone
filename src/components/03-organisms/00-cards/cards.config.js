47cf5dfd2b9f9d74493bed1106a2b06ea25248b5
Ned Zimmerman <ned@bight.ca>
2019-11-20 20:40:40 -0700

    Add resource card component

---
+++ src/components/03-organisms/00-cards/cards.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	title: 'Cards',
+	status: 'wip',
+	context: {
+		cardsTitle: 'Cards',
+		cards: {
+			resource: {
+				title: 'Data as a common in the sharing economy',
+				publisher: 'CEPN',
+				region: 'France',
+				topics: ['Commons', 'Platform Cooperativism', 'Policy']
+			}
+		},
+	}
+};
