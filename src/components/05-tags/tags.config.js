5a59c396bdd7e4b1ecb97be3fdffbc8a1e52454a
Ned Zimmerman <ned@bight.ca>
2019-11-20 20:15:48 -0700

    Add tags

---
+++ src/components/05-tags/tags.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	title: 'Tags',
+	status: 'wip',
+	context: {
+		tagsTitle: 'Tags',
+		tags: [
+			'Starting a Co-op',
+			'Growing a Co-op',
+			'Platform Cooperativism',
+			'Gig Economy',
+			'Surveillance Capitalism',
+			'Movement Building'
+		],
+	}
+};
