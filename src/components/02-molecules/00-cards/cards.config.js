6cd1f4bb55c015f7449106c2e0223095b378e4e9
Ned Zimmerman <ned@bight.ca>
2019-11-21 13:23:54 -0700

    Tweak: label external links

--- src/components/02-molecules/00-cards/cards.config.js
+++ src/components/02-molecules/00-cards/cards.config.js
@@ -5,6 +5,7 @@ module.exports = {
 		cardsTitle: 'Cards',
 		cards: {
 			resource: {
+				id: 1,
 				format: 'Article',
 				title: 'Data as a common in the sharing economy',
 				byline: 'CEPN',
@@ -14,12 +15,14 @@ module.exports = {
 				link: 'resource'
 			},
 			blog: {
+				id: 2,
 				format: 'Blog',
 				title: 'Researching Worker-Owned Platforms',
 				date: 'Apr 13, 2019',
 				link: 'blog'
 			},
 			news: {
+				id: 3,
 				format: 'News',
 				title: 'Platform co-ops: &lsquo;Something new is taking off&rsquo;',
 				byline: 'Co-operative News',
@@ -27,12 +30,14 @@ module.exports = {
 				externalLink: 'https://example.com/news'
 			},
 			publication: {
+				id: 4,
 				format: 'Publication',
 				title: 'Perspectives on Harmful Speech Online',
 				date: 'Jun 27, 2017',
 				link: 'publication'
 			},
 			event: {
+				id: 5,
 				format: 'Community Event',
 				title: 'Toronto Platform Coop Meetup',
 				date: 'Feb 23, 2019, 6:30-8:30PM',
@@ -40,6 +45,7 @@ module.exports = {
 				link: 'event'
 			},
 			platformcoop: {
+				id: 6,
 				format: 'Platform Co-op',
 				title: 'Aarhus Makers',
 				locality: 'Aarhus, Denmark',
