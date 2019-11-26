4f0fc92604531f43a65ef0407f3aac6072fe1178
Ned Zimmerman <ned@bight.ca>
2019-11-26 16:28:38 -0700

    Add standard text input

---
+++ src/components/01-atoms/08-input/input.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: 'Text Input',
+	status: 'wip',
+	context: {
+		label: 'Text Input',
+		name: 'text',
+		placeholder: 'Example input',
+		description: 'Description for this text input.',
+		badValue: 'Incorrect input',
+		error: 'Sorry, that appears to be incorrect.'
+	}
+};
