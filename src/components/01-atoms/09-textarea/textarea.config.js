63beec6c298af8bd26c04875516df4ea2577cf33
Ned Zimmerman <ned@bight.ca>
2019-11-26 21:15:11 -0700

    Add textarea (resolves #54)

---
+++ src/components/01-atoms/09-textarea/textarea.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	title: 'Text Area',
+	status: 'wip',
+	context: {
+		inputTitle: 'Text Areas',
+		label: 'Text Area',
+		name: 'text',
+		placeholder: 'Content goes here.',
+		description: 'Description for this text area.',
+		badValue: 'Incorrect input!',
+		error: 'Sorry, that appears to be incorrect.'
+	}
+};
