eab496de7706db9f76074d955f7113a040206671
Ned Zimmerman <ned@bight.ca>
2019-11-26 21:42:50 -0700

    Add search (resolves #56)

---
+++ src/components/01-atoms/10-search/search.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: 'Search Input',
+	status: 'wip',
+	context: {
+		title: 'Search Input',
+		label: 'Search Input',
+		name: 's',
+		placeholder: 'Example input',
+		description: 'Description for this search input.'
+	}
+};
