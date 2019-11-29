f917650b5988a8890116d55044cb318541e96646
Ned Zimmerman <ned@bight.ca>
2019-11-28 17:35:04 -0700

    Add accordions, first pass

--- src/assets/scripts/pinecone.js
+++ src/assets/scripts/pinecone.js
@@ -19,47 +19,52 @@ icons.forEach( icon => {
 		} );
 } );
 
+// Menu
+
 const menuToggle = document.querySelector( '.menu-toggle' );
 const primaryMenu = document.querySelector( '.menu' );
-const topLevelMenuItems = document.querySelectorAll( '.menu > li > *' );
-const parentMenus = primaryMenu.querySelectorAll( '.menu__submenu-wrapper' );
 
-menuToggle.onclick = () => {
-	const currentState = menuToggle.getAttribute( 'aria-expanded' );
-	const newState = ( 'true' !== currentState );
-	menuToggle.setAttribute( 'aria-expanded', newState );
-};
+if ( menuToggle && primaryMenu ) {
+	const topLevelMenuItems = document.querySelectorAll( '.menu > li > *' );
+	const parentMenus = primaryMenu.querySelectorAll( '.menu__submenu-wrapper' );
 
-Array.prototype.forEach.call( parentMenus, parentMenu => {
-	const linkEl = parentMenu.querySelector( 'a' );
-	const menuButton = document.createElement( 'button' );
-	menuButton.className = 'menu__item';
-	menuButton.setAttribute( 'aria-expanded', false );
-	menuButton.innerHTML = `${linkEl.innerHTML}<svg class="icon icon--chevron-s" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9.8" height="5.886" viewBox="0 0 9.8 5.886"><path d="M15.3,10.3a1,1,0,0,1,1.4,1.4l-4,4a1,1,0,0,1-1.4,0l-4-4a1,1,0,0,1,1.4-1.4L12,13.59l3.3-3.3Z" transform="translate(-7.1 -10.1)" fill="currentColor" fill-rule="evenodd"/></svg>
-	`;
-	parentMenu.insertBefore( menuButton, parentMenu.firstChild );
-	parentMenu.removeChild( linkEl );
-	menuButton.addEventListener( 'click', () => {
-		const currentState = menuButton.getAttribute( 'aria-expanded' );
+	menuToggle.onclick = () => {
+		const currentState = menuToggle.getAttribute( 'aria-expanded' );
 		const newState = ( 'true' !== currentState );
-		menuButton.setAttribute( 'aria-expanded', newState );
-	} );
-	document.addEventListener( 'click', event => {
-		if ( ! parentMenu.contains( event.target ) ) {
-			menuButton.setAttribute( 'aria-expanded', false );
-		}
-	} );
-} );
+		menuToggle.setAttribute( 'aria-expanded', newState );
+	};
 
-Array.prototype.forEach.call( topLevelMenuItems, topLevelMenuItem => {
-	topLevelMenuItem.addEventListener( 'focus', () => {
-		const openDropDown = primaryMenu.querySelector( '[aria-expanded="true"]' );
-		if ( ! openDropDown ) {
-			return;
-		}
-		openDropDown.setAttribute( 'aria-expanded', false );
+	Array.prototype.forEach.call( parentMenus, parentMenu => {
+		const linkEl = parentMenu.querySelector( 'a' );
+		const menuButton = document.createElement( 'button' );
+		menuButton.className = 'menu__item';
+		menuButton.setAttribute( 'aria-expanded', false );
+		menuButton.innerHTML = `${linkEl.innerHTML}<svg class="icon icon--chevron-s" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9.8" height="5.886" viewBox="0 0 9.8 5.886"><path d="M15.3,10.3a1,1,0,0,1,1.4,1.4l-4,4a1,1,0,0,1-1.4,0l-4-4a1,1,0,0,1,1.4-1.4L12,13.59l3.3-3.3Z" transform="translate(-7.1 -10.1)" fill="currentColor" fill-rule="evenodd"/></svg>
+		`;
+		parentMenu.insertBefore( menuButton, parentMenu.firstChild );
+		parentMenu.removeChild( linkEl );
+		menuButton.addEventListener( 'click', () => {
+			const currentState = menuButton.getAttribute( 'aria-expanded' );
+			const newState = ( 'true' !== currentState );
+			menuButton.setAttribute( 'aria-expanded', newState );
+		} );
+		document.addEventListener( 'click', event => {
+			if ( ! parentMenu.contains( event.target ) ) {
+				menuButton.setAttribute( 'aria-expanded', false );
+			}
+		} );
 	} );
-} );
+
+	Array.prototype.forEach.call( topLevelMenuItems, topLevelMenuItem => {
+		topLevelMenuItem.addEventListener( 'focus', () => {
+			const openDropDown = primaryMenu.querySelector( '[aria-expanded="true"]' );
+			if ( ! openDropDown ) {
+				return;
+			}
+			openDropDown.setAttribute( 'aria-expanded', false );
+		} );
+	} );
+}
 
 // Props Heydon Pickering (https://inclusive-components.design/cards/)
 
@@ -77,3 +82,38 @@ Array.prototype.forEach.call( cards, card => {
 		}
 	};
 } );
+
+// Props Heydon Pickering (https://inclusive-components.design/collapsible-sections/)
+
+( function() {
+	// Get all the <h2> headings
+	const headings = document.querySelectorAll( '.accordion__heading' );
+
+	Array.prototype.forEach.call( headings, heading => {
+		const contents = heading.nextElementSibling;
+		const btn = document.createElement( 'button' );
+		btn.setAttribute( 'class', 'accordion__control' );
+		btn.setAttribute( 'aria-expanded', 'false' );
+		btn.innerHTML = `
+			${heading.textContent}
+		  	<svg aria-hidden="true" focusable="false" viewBox="0 0 10 10">
+				<rect class="vert" height="8" width="2" y="1" x="4"/>
+				<rect height="2" width="8" y="4" x="1"/>
+		  	</svg>
+		`;
+		heading.parentNode.insertBefore( btn, heading.nextElementSibling );
+		heading.parentNode.removeChild( heading );
+
+		contents.hidden = true;
+
+		btn.onclick = () => {
+			// Cast the state as a boolean
+			const expanded = 'true' === btn.getAttribute( 'aria-expanded' ) || false;
+
+			// Switch the state
+			btn.setAttribute( 'aria-expanded', !expanded );
+			// Switch the content's visibility
+			contents.hidden = expanded;
+		};
+	} );
+} )();
---
+++ src/components/02-molecules/02-accordion/accordion.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	title: 'Accordion',
+	status: 'wip',
+	context: {
+		accordionLabel: 'Accordion',
+		accordionContent: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+	},
+	variants: [
+		{
+			name: 'Light on Dark',
+			config: {
+				modifier: 'has-dark-mint-500-background-color'
+			}
+		}
+	]
+};
