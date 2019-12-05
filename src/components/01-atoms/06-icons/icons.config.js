55610f22aa5de7c5abfec7ca5a0f53169cb1ae72
Ned Zimmerman <ned@bight.ca>
2019-12-05 15:55:45 -0700

    Overhaul icon system

--- src/components/01-atoms/06-icons/icons.config.js
+++ src/components/01-atoms/06-icons/icons.config.js
@@ -4,12 +4,51 @@ module.exports = {
 	context: {
 		iconsTitle: 'Icons',
 		icons: [
-			'academic-paper',
-			'add',
-			'article',
-			'audio',
-			'book',
-			'broken-link'
+			[
+				'favourite',
+				'share',
+				'edit',
+				'warning',
+				'error',
+				'location',
+				'broken-link',
+				'menu',
+				'filter',
+				'tag',
+				'delete',
+				'settings',
+				'export',
+				'external',],[
+				'chevron-left',
+				'chevron-right',
+				'chevron-up',
+				'chevron-down',
+				'scroll-up',
+				'scroll-down',
+				'close',
+				'add',
+				'calendar',
+				'search',
+			],
+			[
+				'audio',
+				'case-study',
+				'toolkit',
+				'educational-curriculum',
+				'video',
+				'article',
+				'book',
+				'academic-paper',
+				'report',
+				'presentation',
+				'online-training',
+			],
+			[
+				// 'topic',
+				// 'goal',
+				// 'sector',
+				// 'format',
+			]
 		],
 	}
 };
