5d4f982689d3ca4fe14cd220d66d60b4544efaf8
Ned Zimmerman <ned@bight.ca>
2019-12-10 09:41:31 -0700

    Rough prototype of filter list

--- src/components/02-molecules/05-filter-list/filter-list.config.js
+++ src/components/02-molecules/05-filter-list/filter-list.config.js
@@ -2,6 +2,42 @@ module.exports = {
 	name: 'Filter List',
 	status: 'wip',
 	context: {
+		label: 'Topics',
+		terms: [
+			{
+				label: 'Cooperative essentials',
+				childrenCount: 5
+			},
+			{
+				label: 'Business operations',
+				childrenCount: 5
+			},
+			{
+				label: 'Technology & platforms',
+				children: [
+					'Software development',
+					'Platforms design',
+					'Free software',
+					'Data',
+					'Algorithms',
+					'Automation',
+					'Blockchain',
+					'Search engines'
+				]
+			},
+			{
+				label: 'Law & policy',
+				childrenCount: 5
+			},
+			{
+				label: 'Fair labour',
+				childrenCount: 5
+			},
+			{
+				label: 'Platform co-op theory',
+				childrenCount: 5
+			}
+		],
 		scriptCall: 'window.pinecone.accordions()'
 	}
 };
