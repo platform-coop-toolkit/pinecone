24a838308aa2890231a595a0533289122f3a7b4c
Ned Zimmerman <ned@bight.ca>
2019-12-16 22:07:49 -0700

    Add filter list component

--- src/assets/scripts/Pinecone/index.js
+++ src/assets/scripts/Pinecone/index.js
@@ -1,6 +1,7 @@
 import Accordion from './Accordion/index.js';
 import Card from './Card/index.js';
+import FilterList from './FilterList/index.js';
 import Icon from './Icon/index.js';
 import Menu from './Menu/index.js';
 
-export default { Accordion, Card, Icon, Menu };
+export default { Accordion, Card, FilterList, Icon, Menu };
--- src/assets/scripts/pinecone.js
+++ src/assets/scripts/pinecone.js
@@ -30,3 +30,66 @@ if ( accordions ) {
 		new Pinecone.Accordion( accordion );
 	} );
 }
+
+const filterContainer = document.querySelector( '.filter-sort__filters' );
+const showFilters = document.querySelector( '#show-filters' );
+const hideFilters = document.querySelector( '#hide-filters' );
+
+if ( showFilters && hideFilters && filterContainer ) {
+	new Pinecone.FilterList( filterContainer, showFilters, hideFilters );
+}
+
+/**
+ * Show results.
+ */
+// const showResults = function() {
+// 	const slugify = require( 'slugify' );
+// 	const resources = document.querySelectorAll( '.card' );
+// 	const checkedFormats = Array.from( document.querySelectorAll( '.input-group.format [type="checkbox"]:checked' ) );
+// 	if ( 0 < checkedFormats.length ) {
+// 		const resourceList = document.querySelector( '.resource-list' );
+// 		const fmts = checkedFormats.map( format => format.value );
+// 		const fmtLabels = checkedFormats.map( format => document.querySelector( `[for="${format.id}"]` ).innerText );
+// 		Array.prototype.forEach.call( resources, resource => {
+// 			if ( fmts.includes( resource.getAttribute( 'data-format' ) ) ) {
+// 				resource.removeAttribute( 'hidden' );
+// 			} else {
+// 				resource.hidden = true;
+// 			}
+// 		} );
+
+// 		const currentTags = document.querySelector( '.tag-buttons' );
+// 		if ( currentTags ) {
+// 			currentTags.parentNode.removeChild( currentTags );
+// 		}
+
+// 		const selectedTags = document.createElement( 'ul' );
+// 		selectedTags.className = 'tag-buttons';
+// 		Array.prototype.forEach.call( fmtLabels, label => {
+// 			const li = document.createElement( 'li' );
+// 			li.className = 'tag-button';
+// 			const btn = document.createElement( 'button' );
+// 			btn.className = 'tag-button__button';
+// 			btn.setAttribute( 'data-id', `format-${slugify( label, {lower: true} )}` );
+// 			btn.innerHTML = `
+// 					<span class="screen-reader-text">Remove </span>${label} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="icon icon-close" aria-hidden="true"><g fill="none" stroke="#203131" stroke-linecap="round" stroke-miterlimit="10" class="stroke" stroke-width="2"><path d="m5 15 10-10"></path><path d="m15 15-10-10"></path></g></svg>
+// 				`;
+// 			li.appendChild( btn );
+// 			selectedTags.appendChild( li );
+// 		} );
+// 		resourceList.insertBefore( selectedTags, resourceList.firstChild );
+// 	} else {
+// 		Array.prototype.forEach.call( resources, resource => {
+// 			resource.removeAttribute( 'hidden' );
+// 		} );
+
+// 		const currentTags = document.querySelector( '.tag-buttons' );
+// 		if ( currentTags ) {
+// 			currentTags.parentNode.removeChild( currentTags );
+// 		}
+// 	}
+// };
+
+// const applyButton = document.getElementById( 'apply-filters' );
+// applyButton.addEventListener( 'click', showResults );
+
