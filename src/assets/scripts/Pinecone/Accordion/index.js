ac4dcec6cb22acbed3ff090635aba9347be68ad6
Ned Zimmerman <ned@bight.ca>
2019-12-17 16:59:59 -0700

    Adjust method name

--- src/assets/scripts/Pinecone/Accordion/index.js
+++ src/assets/scripts/Pinecone/Accordion/index.js
@@ -22,7 +22,7 @@ class Accordion {
 		Array.prototype.forEach.call( this.panes, pane => {
 			this.initPane( pane );
 		} );
-		this.handleClicks = this.handleClicks.bind( this );
+		this.handleClick = this.handleClick.bind( this );
 		this.addEventListeners();
 	}
 
@@ -49,7 +49,7 @@ class Accordion {
 	 *
 	 * @param {Event} event
 	 */
-	handleClicks( event ) {
+	handleClick( event ) {
 		if ( ! event.target.closest( this.config.controlSelector ) ) return;
 
 		const ctrl = event.target.closest( this.config.controlSelector );
@@ -69,7 +69,7 @@ class Accordion {
 	 * Add click event listeners.
 	 */
 	addEventListeners() {
-		this.container.addEventListener( 'click', this.handleClicks, false );
+		this.container.addEventListener( 'click', this.handleClick, false );
 	}
 }
 
