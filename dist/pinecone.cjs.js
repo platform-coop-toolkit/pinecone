4fd22f659f156d9065754ea017d8c310eb5e9476
Ned Zimmerman <ned@bight.ca>
2019-12-17 21:32:34 -0700

    Preliminary setup for deselect all buttons

--- dist/pinecone.cjs.js
+++ dist/pinecone.cjs.js
@@ -412,7 +412,7 @@ class NestedCheckbox {
 	constructor( container, input, subInputs, options ) {
 		this.container = container;
 		this.input = input;
-		this.inputId = this.input.getAttribute( 'id' );
+		this.inputId = this.input.id;
 		this.customCheckbox = false;
 		this.value = this.input.getAttribute( 'value' );
 		this.label = this.input.nextElementSibling;
@@ -439,7 +439,7 @@ class NestedCheckbox {
 	 */
 	initDisclosure() {
 		const disclosureLabel = this.label.nextElementSibling;
-		const disclosureLabelId = disclosureLabel.getAttribute( 'id' );
+		const disclosureLabelId = disclosureLabel.id;
 		const disclosureBtn = document.createElement( 'button' );
 		disclosureBtn.classList.add( 'disclosure-button' );
 		disclosureBtn.setAttribute( 'type', 'button' );
