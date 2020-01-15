956880ba61e4b0df65b1da6131886ece65bf22c9
Ned Zimmerman <ned@bight.ca>
2020-01-15 09:19:43 -0700

    feat: add new search bar (resolves #117) (#144)
    * feat: move search component into molecules
    * feat: adjust search input style
    * feat: rename inverse variant
    * feat: implement states
    * fix: align with WordPress search form pattern
    * fix: icon alignment
    * fix: align focus state with text inputs

--- src/components/02-molecules/07-search/search.config.js
+++ src/components/02-molecules/07-search/search.config.js
@@ -5,14 +5,16 @@ module.exports = {
 		standAlone: false,
 		name: 's',
 		placeholder: 'Search…',
-		label: 'Search'
+		label: 'search',
+		submitLabel: 'submit search'
 	},
 	variants: [
 		{
-			name: 'Default (Light on Dark)',
-			label: 'Default (Light on Dark)',
+			name: 'Inverse',
+			label: 'Inverse',
 			context: {
-				bodyClass: 'has-dark-mint-500-background-color'
+				modifier: 'inverse',
+				bodyClass: 'has-blue-700-background-color'
 			}
 		}
 	]
