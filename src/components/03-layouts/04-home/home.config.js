1fb1f48de7b747da65e92c26ee90e7cb3f775e88
Ned Zimmerman <ned@bight.dev>
2020-01-17 09:11:49 -0700

    feat: add home layout

---
+++ src/components/03-layouts/04-home/home.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	title: 'Home',
+	status: 'wip',
+	context: {
+		bodyClass: 'page home',
+		hasMenu: true,
+		title: '<span class="pc-ff--sans pc-fw--normal">Platform Co-op</span><br /> Resource Library',
+		subhead: 'A place where you can learn and share knowledge about co-ops, platform co-ops, the digital economy, and more.'
+	}
+};
