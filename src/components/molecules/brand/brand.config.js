10c675590e0b83d6b0e4ae919975244ac0117776
Ned Zimmerman <ned@bight.dev>
2020-04-15 13:49:39 -0600

    feat: add library logo

--- src/components/molecules/brand/brand.config.js
+++ src/components/molecules/brand/brand.config.js
@@ -3,8 +3,19 @@ module.exports = {
 	status: 'ready',
 	order: 1,
 	context: {
+		logo: 'pcc',
 		inBanner: true,
 		title: 'Platform Cooperativism Consortium',
 		visuallyHiddenTitle: true,
-	}
+	},
+	variants: [
+		{
+			name: 'resources',
+			label: 'Resource Library',
+			context: {
+				logo: 'resources',
+				title: 'Platform Cooperativism Resource Library'
+			}
+		}
+	]
 };
