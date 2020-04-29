3348f580f056f053c0a91de86ab1f7b30a734ce3
jhung <jhung@ocadu.ca>
2020-04-29 16:22:46 -0400

    Fix/filter parent state (#329)

--- src/components/atoms/form-elements/custom-checkboxes/custom-checkboxes.config.js
+++ src/components/atoms/form-elements/custom-checkboxes/custom-checkboxes.config.js
@@ -7,11 +7,27 @@ module.exports = {
 		label: 'Nested Checkboxes',
 		items: [
 			{
-				label: 'Parent item 1',
+				label: 'Parent item 1 (initially unchecked)',
 				children: [
-					'Sub item 1',
-					'Sub item 2',
-					'Sub item 3'
+					{ label: 'Sub item 1' },
+					{ label: 'Sub item 2' },
+					{ label: 'Sub item 3' }
+				]
+			},
+			{
+				label: 'Parent item 2 (initially mixed)',
+				children: [
+					{ label: 'Sub item 4', checked: true },
+					{ label: 'Sub item 5'},
+					{ label: 'Sub item 6', checked: true }
+				]
+			},
+			{
+				label: 'Parent item 3 (initially all checked)',
+				children: [
+					{ label: 'Sub item 7', checked: true },
+					{ label: 'Sub item 8', checked: true },
+					{ label: 'Sub item 9', checked: true }
 				]
 			}
     ]
